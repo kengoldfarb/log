40158d0b1211b9388a8cc560a8184a0ef0215c2e
Ken <hello@kengoldfarb.com>
2020-07-16 00:12:10 -0400

    latest

--- release.config.js
+++ release.config.js
@@ -1,5 +1,4 @@
 const semanticReleaseConfig = require('@kengoldfarb/semantic-release-config')
-	.default
 
 const config = semanticReleaseConfig.default({
 	config: semanticReleaseConfig.ReleaseConfiguration.Package
