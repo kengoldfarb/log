53fa38daae3a546d6a0ae3832d9f8b46a08ff300
Ken Goldfarb <hello@kengoldfarb.com>
2024-02-15 12:13:31 -0700

    feat: package and build process upgrades

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 // import json from '@rollup/plugin-json'
@@ -6,34 +7,95 @@ import builtins from 'rollup-plugin-node-builtins'
 import globals from 'rollup-plugin-node-globals'
 import replace from 'rollup-plugin-replace'
 
-export default {
-	// input: 'build/index.js',
-	input: 'index.ts',
-	output: {
-		dir: 'build',
-		format: 'umd',
-		name: 'Log',
-		sourcemap: true,
+export default [
+	{
+		// input: 'build/index.js',
+		input: 'index.ts',
+		output: {
+			file: 'build/index.umd.js',
+			format: 'umd',
+			name: 'Log',
+			sourcemap: true
+		},
+		external: ['fs', 'http', 'https', 'child_process'],
+		plugins: [
+			replace({
+				include: ['node_modules/uuid/**'],
+				delimiters: ['', ''],
+				values: {
+					'crypto.randomBytes': "require('randombytes')"
+				}
+			}),
+			typescript({
+				tsconfig: './tsconfig.browser.json'
+			}),
+			resolve({
+				browser: true
+				// preferBuiltins: false
+			}),
+			commonjs({ extensions: ['.js', '.ts'] }),
+			globals(),
+			builtins()
+		]
 	},
-	external: ['fs', 'http', 'https', 'child_process'],
-	plugins: [
-		replace({
-			include: ['node_modules/uuid/**'],
-			delimiters: ['', ''],
-			values: {
-				'crypto.randomBytes': "require('randombytes')",
-			},
-		}),
-		typescript({
-			tsconfig: './tsconfig.browser.json',
-		}),
-		resolve({
-			browser: true,
-			// preferBuiltins: false
-		}),
-		commonjs({ extensions: ['.js', '.ts'] }),
-		globals(),
-		builtins(),
-		// json()
-	],
-}
+	{
+		// input: 'build/index.js',
+		input: 'index.ts',
+		output: {
+			file: 'build/index.cjs.js',
+			format: 'cjs',
+			name: 'Log',
+			sourcemap: true
+		},
+		external: ['fs', 'http', 'https', 'child_process'],
+		plugins: [
+			replace({
+				include: ['node_modules/uuid/**'],
+				delimiters: ['', ''],
+				values: {
+					'crypto.randomBytes': "require('randombytes')"
+				}
+			}),
+			typescript({
+				tsconfig: './tsconfig.json'
+			}),
+			resolve({
+				browser: false
+				// preferBuiltins: false
+			}),
+			commonjs({ extensions: ['.js', '.ts'] }),
+			globals(),
+			builtins()
+		]
+	},
+	{
+		// input: 'build/index.js',
+		input: 'index.ts',
+		output: {
+			file: 'build/index.esm.js',
+			format: 'esm',
+			name: 'Log',
+			sourcemap: true
+		},
+		external: ['fs', 'http', 'https', 'child_process'],
+		plugins: [
+			replace({
+				include: ['node_modules/uuid/**'],
+				delimiters: ['', ''],
+				values: {
+					'crypto.randomBytes': "require('randombytes')"
+				}
+			}),
+			typescript({
+				tsconfig: './tsconfig.json'
+			}),
+			resolve({
+				browser: false
+				// preferBuiltins: false
+			}),
+			commonjs({ extensions: ['.js', '.ts'] }),
+			globals(),
+			builtins()
+		]
+	}
+]
