269d9162735ad83c6135a7cbed31571e4815dffc
Ken Goldfarb <hello@kengoldfarb.com>
2024-05-29 13:32:04 -0600

    debug release

--- release.config.cjs
+++ release.config.cjs
@@ -1,7 +1,14 @@
 const semanticReleaseConfig = require('@kengoldfarb/semantic-release-config')
 
 const config = semanticReleaseConfig.default({
-	config: 'package'
+	config: 'package',
+	branches: [
+		'master',
+		{ name: 'dev', channel: 'beta' },
+		...sharedPrereleaseBranches,
+		{ name: 'prerelease/(.*)', prerelease: true },
+		{ name: 'prerelease-(.*)', prerelease: true }
+	]
 })
 
 module.exports = config
